0e54caca4c210017b358d14d627a22b05b125c0e
Andreas Giereck Kristiansen <giereck@hotmail.com>
2024-05-20 13:01:35 +0200

    Minor refactoring to Day 1.

--- src/puzzle_day_1/day1.rs
+++ src/puzzle_day_1/day1.rs
@@ -8,9 +8,9 @@ pub fn run() {
 
 fn part_one() -> io::Result<()> {
     let lines = get_lines_from_file()?;
-    let numbers: Vec<u32> = get_numbers_from_lines(lines);
+    let numbers: Vec<u8> = get_numbers_from_lines(lines);
 
-    let sum: u32 = numbers.iter().sum();
+    let sum: u32 = numbers.iter().map(|&num| num as u32).sum();
     println!("Answer part 1: {}", sum);
 
     Ok(())
@@ -18,17 +18,16 @@ fn part_one() -> io::Result<()> {
 
 fn part_two() -> io::Result<()> {
     let lines = get_lines_from_file()?;
+    let numbers: Vec<u8> = get_numbers_from_lines_v2(&lines);
 
-    let numbers: Vec<u32> = get_numbers_from_lines_v2(lines);
-
-    let sum: u32 = numbers.iter().sum();
+    let sum: u32 = numbers.iter().map(|&num| num as u32).sum();
     println!("Answer part 2: {}", sum);
 
     Ok(())
 }
 
-fn get_numbers_from_lines(lines: Vec<String>) -> Vec<u32> {
-    let mut numbers: Vec<u32> = Vec::new();
+fn get_numbers_from_lines(lines: Vec<String>) -> Vec<u8> {
+    let mut numbers: Vec<u8> = Vec::new();
     for line in lines {
         let mut number_string = String::new();
 
@@ -54,7 +53,7 @@ fn get_numbers_from_lines(lines: Vec<String>) -> Vec<u32> {
     }
     numbers
 }
-fn get_numbers_from_lines_v2(lines: Vec<String>) -> Vec<u32> {
+fn get_numbers_from_lines_v2(lines: &Vec<String>) -> Vec<u8> {
     let number_vec: Vec<(&str, &str)> = vec![
         ("one", "o1ne"),
         ("two", "t2o"),
@@ -67,38 +66,17 @@ fn get_numbers_from_lines_v2(lines: Vec<String>) -> Vec<u32> {
         ("nine", "n9ne"),
     ];
 
-    let mut numbers: Vec<u32> = Vec::new();
-
+    let mut replaced_lines: Vec<String> = vec![];
     for line in lines {
-        let mut line = line;
+        let mut line: String = String::from(line);
 
         for v in &number_vec {
-            line = String::from(&line.replace(v.0, v.1))
+            line = line.replace(v.0, v.1)
         }
-
-        let mut number_string = String::new();
-        // Loop through line forwards
-        for c in line.chars() {
-            if c.is_numeric() {
-                number_string.push(c);
-                break;
-            }
-        }
-
-        // // Loop through line backwards
-        for c in line.chars().rev() {
-            if c.is_numeric() {
-                number_string.push(c);
-                break;
-            }
-        }
-
-        if number_string.len() > 0 {
-            let number = number_string.parse().unwrap();
-            numbers.push(number);
-        }
+        replaced_lines.push(line);
     }
-    numbers
+
+    get_numbers_from_lines(replaced_lines)
 }
 
 fn get_lines_from_file() -> io::Result<Vec<String>> {
